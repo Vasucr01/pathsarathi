21e1404f8f78118fc184a3fca3993a791557abc8
CHAUHAN _VASU <vasucr01@gmail.com>
2025-11-04 13:28:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Smooth scroll for navigationdocument.querySelectorAll('a[href^="#"]').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
-
-
